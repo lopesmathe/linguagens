eb7498123a5c689a93e71141213849a22f37498b
lopesmathe <matheuslopesbauer@gmail.com>
2023-12-03 01:05:11 -0300

    add aula 9 e 10

---
+++ js/aula09.js
@@ -0,0 +1,7 @@
+let bata=10
+let batas=10
+console.log(bata++ + bata++ + bata)
+
+
+
+
---
+++ js/aula10.js
@@ -0,0 +1,12 @@
+let num1=3
+
+let res=num1%2
+
+if(res==0){
+    console.log("par")
+}
+else{
+    console.log("impar")
+}
+
+res=(!(num1%2) ? "par" : "Impar")
