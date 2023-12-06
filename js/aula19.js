f0c0262435869f0814834d8358f1118b1bc2eca5
lopesmathe <matheuslopesbauer@gmail.com>
2023-12-06 02:52:16 -0300

    add aula 19 ate 20

---
+++ js/aula19.js
@@ -0,0 +1,12 @@
+let n=1000
+let max=500
+let pares=0
+
+for(let i=0; i<max; i++){
+if(i%2==0)
+    continue
+    pares++
+
+}
+console.log("quantidades de pares" + pares)
+console.log("fim")
---
+++ js/aula20.js
@@ -0,0 +1,9 @@
+function mudartxt() {
+  let d1 = document.querySelector(".d1");
+  let d2 = document.querySelector(".d2");
+  let d3 = document.querySelector(".d3");
+
+  d1.innerHTML = "mudaste";
+  d2.innerHTML = "mudaste";
+  d3.innerHTML = "mudaste";
+}
